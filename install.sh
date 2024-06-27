6305f73606ec7c321a613c661c9d90135f1d030d
steven0x0 <160603102+steven0x0@users.noreply.github.com>
2024-06-27 16:07:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-OS=`uname`
+OS=`uname | tr '[:upper:]' '[:lower:]'`
 ARCH=`uname -m`
 
 PACKAGE_VERSION=v0.0.2
-PACKAGE_MD5=3251d05385002b092e72b2db7f7bd4f2
-
-CAPELL_PACKAGE_NAME=capell_edge_server_linux_x86_64_${PACKAGE_VERSION}.zip
+CAPELL_PACKAGE_NAME=capell_edge_server_${OS}_${ARCH}_${PACKAGE_VERSION}.zip
 CAPELL_PACKAGE_PATH=/tmp/$CAPELL_PACKAGE_NAME
 
 function quitWithError() {
@@ -14,21 +12,11 @@ function quitWithError() {
     exit 1
 }
 
-function downloadPackage() {
-    echo "download package: $CAPELL_PACKAGE_NAME"
-    curl -#L -o $CAPELL_PACKAGE_PATH "https://github.com/capell-io/service-node/releases/download/$PACKAGE_VERSION/$CAPELL_PACKAGE_NAME"
 
-    CHECKSUM=`md5sum "$CAPELL_PACKAGE_PATH"|cut -d" " -f1`
-    if [ $CHECKSUM != $PACKAGE_MD5 ]; then
-        rm -f $CAPELL_PACKAGE_PATH
-        quitWithError "md5 unmatch: $CHECKSUM,$PACKAGE_MD5"
-    fi
-}
-
-if [ x"$OS" != x"Linux" ]; then
-    quitWithError "only supports linux system"
+if [ x"$OS" != x"linux" ] && [ x"$OS" != x"darwin" ]; then
+    quitWithError "system not supported"
 fi
-if [ x"$ARCH" != x"x86_64" ]; then
+if [ x"$ARCH" != x"x86_64" ] && [ x"$OS" == x"linux" ]; then
     quitWithError "only supports x86_64"
 fi
 if [ x"$UID" != x"0" ]; then
@@ -39,58 +27,59 @@ if [ x"$BIND" == x"" ]; then
 fi
 
 if [ -f $CAPELL_PACKAGE_PATH ]; then
-    CHECKSUM=`md5sum "$CAPELL_PACKAGE_PATH"|cut -d" " -f1`
-    if [ $CHECKSUM == $PACKAGE_MD5 ]; then
-        echo "found local package, skip download"
-    else
-        rm -f $CAPELL_PACKAGE_PATH
-        downloadPackage
-    fi
-else
-    downloadPackage
+    rm -f $CAPELL_PACKAGE_PATH
 fi
 
-if ! unzip -v > /dev/null 2>&1; then
-    source /etc/os-release
-    case $ID in
-        debian|ubuntu)
-            apt update
-            apt install -y unzip
-            ;;
-        centos|fedora|rhel)
-            yum update
-            yum install -y unzip
-            ;;
-        *)
-            quitWithError "$ID is not supported"
-            ;;
-    esac
+echo "download package: $CAPELL_PACKAGE_NAME"
+curl -#L -o $CAPELL_PACKAGE_PATH "https://github.com/capell-io/service-node/releases/download/$PACKAGE_VERSION/$CAPELL_PACKAGE_NAME"
+
+if [ x"$OS" == x"Linux" ]; then
+    if ! unzip -v > /dev/null 2>&1; then
+        source /etc/os-release
+        case $ID in
+            debian|ubuntu)
+                apt update
+                apt install -y unzip
+                ;;
+            centos|fedora|rhel)
+                yum update
+                yum install -y unzip
+                ;;
+            *)
+                quitWithError "$ID is not supported"
+                ;;
+        esac
+    fi
 fi
 
 echo "install ..."
 unzip -o $CAPELL_PACKAGE_PATH
 
-SERVICE_CONF=/usr/lib/systemd/system/capell.service
+if [ x"$OS" == x"Linux" ]; then
+    SERVICE_CONF=/usr/lib/systemd/system/capell.service
 
-if [ -f $SERVICE_CONF ]; then
-    echo -e "\033[33mwarnning: service already exists\033[0m"
+    if [ -f $SERVICE_CONF ]; then
+        echo -e "\033[33mwarnning: service already exists\033[0m"
+    fi
+
+    echo "[Unit]
+    Description=Capell edge node
+    After=libvirtd.service docker.service
+
+    [Service]
+    Type=simple
+    User=root
+    Group=root
+    ExecStart=`pwd`/capell-edge-server --bind $BIND
+    Restart=on-failure
+
+    [Install]
+    WantedBy=multi-user.target" > $SERVICE_CONF
+
+    systemctl enable capell.service
+    systemctl restart capell.service
+else
+    ./capell-edge-server --bind $BIND -d
 fi
-
-echo "[Unit]
-Description=Capell edge node
-After=libvirtd.service docker.service
-
-[Service]
-Type=simple
-User=root
-Group=root
-ExecStart=`pwd`/capell-edge-server --bind $BIND
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target" > $SERVICE_CONF
-
-systemctl enable capell.service
-systemctl restart capell.service
 
 echo -e "\033[32minstallation is completed\033[0m"
